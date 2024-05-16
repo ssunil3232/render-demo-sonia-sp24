85b787bc5f147cb451f7873414237f358f990f48
Sonia Sunil <37371431+ssunil3232@users.noreply.github.com>
2024-05-16 03:54:09 +0000

    fix render

--- app.py
+++ app.py
@@ -4,25 +4,25 @@ from whitenoise import WhiteNoise
 app = Flask(__name__)
 
 @app.route('/')
-def intro(): 
+def index(): 
     return render_template("intro.html")
 
 app.wsgi_app = WhiteNoise(app.wsgi_app, 
                           root='project1/', 
                           prefix='/project1/', 
-                          index_file='intro.html', 
+                          index_file='index.htm', 
                           autorefresh=True)
 
 app.wsgi_app = WhiteNoise(app.wsgi_app, 
                           root='project2/', 
                           prefix='/project2/', 
-                          index_file='intro.html', 
+                          index_file='index.htm', 
                           autorefresh=True)
 
 app.wsgi_app = WhiteNoise(app.wsgi_app, 
                           root='project3/', 
                           prefix='/project3/', 
-                          index_file='intro.html', 
+                          index_file='index.htm', 
                           autorefresh=True)
 
 
