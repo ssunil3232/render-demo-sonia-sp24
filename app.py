393fa5675f8e0ae69bd433d1df30dcc588a1b15f
qadan96 <bq55@cornell.edu>
2024-05-15 23:35:26 -0400

    intro page code

--- app.py
+++ app.py
@@ -4,25 +4,25 @@ from whitenoise import WhiteNoise
 app = Flask(__name__)
 
 @app.route('/')
-def index(): 
-    return render_template("home.html")
+def intro(): 
+    return render_template("intro.html")
 
 app.wsgi_app = WhiteNoise(app.wsgi_app, 
                           root='project1/', 
                           prefix='/project1/', 
-                          index_file='index.htm', 
+                          index_file='intro.html', 
                           autorefresh=True)
 
 app.wsgi_app = WhiteNoise(app.wsgi_app, 
                           root='project2/', 
                           prefix='/project2/', 
-                          index_file='index.htm', 
+                          index_file='intro.html', 
                           autorefresh=True)
 
 app.wsgi_app = WhiteNoise(app.wsgi_app, 
                           root='project3/', 
                           prefix='/project3/', 
-                          index_file='index.htm', 
+                          index_file='intro.html', 
                           autorefresh=True)
 
 
